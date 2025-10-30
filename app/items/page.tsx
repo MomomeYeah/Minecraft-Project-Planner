de89146f7d987b063cb8e5de5bfe6c72f5e850c0
Robert Gwynn-Jones <gwynnr@gmail.com>
2025-10-30 22:36:16 +1100

    Replace items loading.tsx with per-component suspense

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import Search from "@/app/ui/search";
 import { fetchAllItems, fetchAllItemsPages } from "@/app/lib/data";
 import { SearchPagination } from "../ui/pagination";
+import { Skeleton } from "@/components/ui/skeleton"
 import {
   Table,
   TableBody,
@@ -10,6 +12,51 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+function PageSkeleton() {
+    const iterator = Array.from({ length: 10 }, (_, i) => i);
+
+    return (
+        <TableBody>
+            {iterator.map((index) => (
+                <TableRow key={index}>
+                    <TableCell className="font-medium"><Skeleton className="h-6 w-1/2 mb-2" /></TableCell>
+                    <TableCell className="text-right"><Skeleton className="h-6 w-1/2 mb-2 float-right" /></TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    );
+}
+
+async function PageData(props: PageProps) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const items = await fetchAllItems(query, currentPage);
+
+    return (
+        <TableBody>
+            {items.map((item) => (
+                <TableRow key={item.id}>
+                    <TableCell className="font-medium">{item.name}</TableCell>
+                    <TableCell className="text-right">{item.item_id}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    );
+}
+
+async function PagePagination(props: PageProps) {
+    const searchParams = await props.searchParams;
+    const currentPage = Number(searchParams?.page) || 1;
+    const query = searchParams?.query || "";
+    const totalPages = await fetchAllItemsPages(query);
+
+    return (
+        <SearchPagination currentPage={currentPage} totalPages={totalPages} />
+    );
+}
+
 type PageProps = {
     searchParams?: Promise<{
         query?: string;
@@ -17,34 +64,24 @@ type PageProps = {
     }>
 }
 export default async function Page(props: PageProps) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || "";
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchAllItemsPages(query);
-
-    const items = await fetchAllItems(query, currentPage);
-    
     return (
         <>
             <Search placeholder="Search items..." />
-            <Table>
+            <Table className="mt-4">
                 <TableHeader>
                     <TableRow>
                         <TableHead>Name</TableHead>
                         <TableHead className="text-right">Item ID</TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
-                    {items.map((item) => (
-                        <TableRow key={item.id}>
-                            <TableCell className="font-medium">{item.name}</TableCell>
-                            <TableCell className="text-right">{item.item_id}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
+                <Suspense fallback={<PageSkeleton />}>
+                    <PageData searchParams={props.searchParams} />
+                </Suspense>
             </Table>
             <div className="mt-5 flex w-full justify-center">
-                <SearchPagination currentPage={currentPage} totalPages={totalPages} />
+                <Suspense fallback={null}>
+                    <PagePagination searchParams={props.searchParams} />
+                </Suspense>
             </div>
         </>
     );
